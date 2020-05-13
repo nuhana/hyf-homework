7f2331f08f71a8960aabf5d9f264c0c26bf31c09
nouha <nuhanajah90-gmail.com>
2020-05-14 00:43:31 +0200

    implemented feedback for js1 w1 task1

--- javascript/javascript1/week1/ez-namey.js
+++ javascript/javascript1/week1/ez-namey.js
@@ -1,11 +1,34 @@
-const firstWords=["easy", "awesome", "amazing", "beautiful",
- "business", "best", "bare", "good", "perfect", "primary"];
-const secondWords=["Corporation", "motivation", "plan", "start", "patners", 
-"office", "five", "hands", "home", "profit" ];
+const firstWords = [
+  "easy",
+  "awesome",
+  "amazing",
+  "beautiful",
+  "business",
+  "best",
+  "bare",
+  "good",
+  "perfect",
+  "primary",
+];
+const secondWords = [
+  "Corporation",
+  "motivation",
+  "plan",
+  "start",
+  "patners",
+  "office",
+  "five",
+  "hands",
+  "home",
+  "profit",
+];
 
 const randomNumber = Math.floor(Math.random() * 10) + 0;
 
-const startupName=firstWords[randomNumber]+ ' ' +secondWords[randomNumber];
-const length=firstWords[randomNumber].length +secondWords[randomNumber].length;
+const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+const length =
+  firstWords[randomNumber].length + secondWords[randomNumber].length;
 
-console.log(' the statrtup ' + startupName +' contains' +' ' + length +' characers');
+console.log(
+  " the statrtup " + startupName + " contains" + " " + length + " characers"
+);
