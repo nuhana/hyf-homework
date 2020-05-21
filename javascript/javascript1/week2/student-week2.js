5ad01fb39bc59ca0e903f2fa7bfae9535cae7b9e
nouha <nuhanajah90-gmail.com>
2020-05-22 01:36:10 +0200

    student manager

--- javascript/javascript1/week2/student-week2.js
+++ javascript/javascript1/week2/student-week2.js
@@ -2,30 +2,22 @@ const class07Students = [];
 function addStudentToClass(studentName) {
   let length = class07Students.length;
 
-  let i = 0;
-  if (studentName === "queen") {
-    let m = class07Students.push("queen");
-    return;
-  } else if (length > 5) {
-    alert("Cannot add more students to class 07");
-    return;
-  } else if (class07Students[i] === studentName) {
-    alert("Student" + " " + studentName + " is already in the class");
-    i++;
-    return;
-  } else if (studentName === "") {
-    alert("this is not a name");
-    return;
-  } else {
-    let s = class07Students.push(studentName);
+  if (length > 5 && studentName !== "queen") {
+    console.log("Cannot add more students to class 07");
     return;
   }
+  if (studentName.trim() === "") {
+    console.log("this is not a name");
+    return;
+  }
+  if (class07Students.includes(studentName)) {
+    console.log("Student" + " " + studentName + " is already in the class");
+    return;
+  }
+  class07Students.push(studentName);
+  return;
 }
+
 function getNumberOfStudents() {
-  let numberOfStudents = class07Students.length;
-  for (let i = 1; i <= numberOfStudents; i++) {
-    console.log(class07Students[i]);
-  }
-  return numberOfStudents;
+  return class07Students.length;
 }
-getNumberOfStudents();
