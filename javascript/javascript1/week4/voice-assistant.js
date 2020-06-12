42c7ec904b983ad701d284181e04a874c247ed14
nouha <nuhanajah90-gmail.com>
2020-06-12 10:05:38 +0200

    feedback

--- javascript/javascript1/week4/voice-assistant.js
+++ javascript/javascript1/week4/voice-assistant.js
@@ -1,38 +1,52 @@
-let userName = "nuha";
-let todo = [];
-let date = new Date("2020-06-01");
+let todos = [];
+let date = new Date();
 var options = {
   day: "numeric",
   month: "long",
   year: "numeric",
 };
+
 function getReply(command) {
-  if (command.includes("Hello")) {
-    return "nice to meet you " + userName;
+  if (command.startsWith("hello my name is ")) {
+    const name = command.replace("hello my name is", "");
+    return "Nice to meet you " + name;
   } else if (command.includes("What is my name")) {
-    return "your name is " + userName;
-  } else if (command.includes("Add fishing to my todo")) {
-    todo.push(command);
-    return "fishing added to your todo";
-  } else if (command.includes("Add singing in the shower to my todo")) {
-    todo.push(command);
-    return "singing added to your todo";
-  } else if (command.includes("Remove fishing from my todo")) {
-    let removeCommand = "Add fishing to my todo";
-    for (let i = 0; i < todo.length; i++) {
-      if (todo[i] === removeCommand) {
-        todo.splice(i, 1);
+    return "your name is " + name;
+  } else if (command.startsWith("add") && command.endsWith("to my to-do")) {
+    const todo = command.replace("add", "").replace(" to my to-do", "");
+    todos.push(todo);
+    return todo + "added to your to-do";
+  } else if (command.startsWith("Remove") && command.endsWith("from my todo")) {
+    const activityList = command
+      .replace("Remove", "")
+      .replace("from my todo", "");
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i] === activityList) {
+        todos.splice(i, 1);
       }
     }
-    return "Removed fishing from your todo";
+    return activityList + " removed from your todo";
   } else if (command.includes("What is on my todo?")) {
-    return "you have " + todo.length + " todos - " + todo;
+    return "you have " + todos.length + " todos - " + todos;
   } else if (command.includes("What day is it today?")) {
     return date.toLocaleDateString("en-ZA", options);
-  } else if (command.includes("3 + 3")) {
-    return "6";
-  } else if (command.includes("4 * 12")) {
-    return "48";
+  } else if (command.startsWith("what is")) {
+    const commandWords = command.split(" ");
+    const number1 = parseInt(commandWords[2]);
+    const number2 = parseInt(commandWords[4]);
+    const operation = commandWords[3];
+    if (number1 && number2) {
+      switch (operation) {
+        case "+":
+          return number1 + number2;
+        case "-":
+          return number1 - number2;
+        case "*":
+          return number1 * number2;
+        case "/":
+          return number1 / number2;
+      }
+    }
   } else if (command.includes("Set a timer for 4 minutes")) {
     setTimeout(function () {
       console.log();
@@ -41,7 +55,7 @@ function getReply(command) {
     return "Timer set for 4 minutes";
   }
 }
-console.log(getReply("Hello my name is " + userName));
+console.log(getReply("hello my name is "));
 console.log(getReply("What is my name"));
 console.log(getReply("Add fishing to my todo"));
 console.log(getReply("Add singing in the shower to my todo"));
