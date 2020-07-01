b0448276f8acf7b1a6870469db211dc6ece5bea6
nuha <nuhanajah90@gmail.com>
2020-07-01 11:30:41 +0200

    my json file

---
+++ javascript/javascript3/week1/my-own-json.js
@@ -0,0 +1,6 @@
+const jsonFavouriteFood =
+  '{"List of dinner":["shawrma","fruits","salat"], "list of lanch":["dolma", "rice", "salat"]}';
+console.log(jsonFavouriteFood);
+
+const objFavouriteFood = JSON.parse(jsonFavouriteFood);
+console.log(objFavouriteFood);
