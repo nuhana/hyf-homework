a6caee583a17909ccad7fb8d0a9053d5408c57af
nuha90 <nuhanajah90@gmail.com>
2020-08-04 11:41:36 +0200

    feedback implemented

--- javascript/javascript3/week3/shopping-cart.js
+++ javascript/javascript3/week3/shopping-cart.js
@@ -2,24 +2,22 @@ class Product {
   constructor(name, price) {
     this.name = name;
     this.price = price;
-    this.crrConvereted;
   }
   convertToCurrency(currency) {
-    this.url =
-      "https://v6.exchangerate-api.com/v6/e46f73f788f19d8087746d89/latest/" +
-      currency;
+    this.crrConvereted;
+    this.url = "https://api.exchangeratesapi.io/latest?base" + currency;
     fetch(this.url)
       .then((response) => response.json())
       .then((ccr) => {
-        this.crrConvereted = this.price / ccr.conversion_rates.DKK;
-        console.log(this.crrConvereted);
+        console.log(ccr);
+        this.crrConvereted = this.price / ccr.rates.DKK;
+        return console.log(this.crrConvereted);
       });
   }
 }
 class ShoppingCart {
   constructor() {
     this.products = [];
-    this.total = 0;
   }
   addProduct(product) {
     this.products.push(product);
@@ -33,6 +31,7 @@ class ShoppingCart {
     );
   }
   getTotal() {
+    this.total = 0;
     for (let i = 0; i < this.products.length; i++) {
       this.total = this.total + this.products[i].price;
     }
