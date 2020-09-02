6d5fb14a3608be1497a4174effd14bcd1ed4a210
nuha90 <nuhanajah90@gmail.com>
2020-09-02 17:53:24 +0200

    conditions updated

--- nodejs/week2/´meal sharing app/src/backend/routes/meals.js
+++ nodejs/week2/´meal sharing app/src/backend/routes/meals.js
@@ -3,15 +3,25 @@ const router = express.Router();
 let meals = require("../data/meals.json");
 
 router.get("/:id", async (request, response) => {
-  response.json(
-    meals.filter(meals => meals.id === parseInt(request.params.id))
-  );
+  const found = meals.some(meals => meals.id === parseInt(request.params.id));
+  if (found) {
+    response.json(
+      meals.filter(meals => meals.id === parseInt(request.params.id))
+    );
+  } else {
+    response.status(400).json("no meals for this id");
+  }
 });
 router.get("/", async (request, response) => {
   if (request.query.title) {
-    response.send(
-      meals.filter(meal => meal.title.includes(request.query.title))
-    );
+    const found = meals.some(meal => meal.title.includes(request.query.title));
+    if (found) {
+      response.send(
+        meals.filter(meal => meal.title.includes(request.query.title))
+      );
+    } else {
+      response.status(400).json("no meals with this title");
+    }
   }
   if (request.query.maxPrice) {
     meals.filter(meal => meal.price <= request.query.maxPrice);
