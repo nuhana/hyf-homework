f49065e4ac1eefb870a492df378a214610d74397
nuha90 <nuhanajah90@gmail.com>
2020-10-17 10:23:22 +0200

    children added

--- react/week3/component/todoItems.js
+++ react/week3/component/todoItems.js
@@ -1,4 +1,5 @@
 import React,{useState} from "react";
+import Children from './children'
 
 const TodoItems = ({ description, todo, deleteTodo,
 handleCompleted ,string, handleEdit}) => {
@@ -14,39 +15,26 @@ const handleSave=id=>{
     handleEdit(editValue,id)
   }
   else{
-    handleEdit(description)
+    setEditValue(description)
   }
 }
 
 if(onEdit){
   return (
-    <div className="todo">
+    <Children>
       <li className= {`todo-item ${todo.completed? 'complete':''}`}>
-     <input type="text" id='editValue' value={editValue} name='editValue'
+     <input type="text" id='editValue' name='editValue'
      onChange={(e)=>setEditValue(e.target.value)}></input>
       </li>
-      <button onClick={() => handleCompleted(todo)}
-        className="complete-btn"
-      >
-        <i className="fa fa-check"></i>
-      </button>
-      <button
-        onClick={() => {
-          deleteTodo(todo);
-        }}
-        className="trash-btn"
-      >
-        <i className="fa fa-trash-o"></i>
-      </button>
       <button className='save-btn' onClick={()=>handleSave(todo.id)}>save</button>
      
-    </div>
+      </Children>
   );
 }else{
   return (
-    <div className="todo">
+    <Children>
       <li className= {`todo-item ${todo.completed? 'complete':''}`}>
-      {editValue} | {string}
+      {description} | {string}
       </li>
 <button onClick={() => handleCompleted(todo)}
         className="complete-btn"
@@ -68,7 +56,7 @@ if(onEdit){
       ><i className="fa fa-edit" ></i>
      </button>
      
-    </div>
+     </Children>
   );
 }
 };
